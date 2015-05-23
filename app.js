662f251af4b1c7e5f98217cb5899805953cd34c7
facu.tk <facu.tk@gmail.com>
2015-05-23 00:34:07 -0300

    node/phantom interaction

--- app.js
+++ app.js
@@ -1,8 +1,43 @@
 var express = require('express');
 var app = express();
+var phantom = require('phantom');
+var fs = require('fs');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+
+app.get('/gen', function(request, response) {
+  fs.writeFile('message', 'gen from phantom', function(error) {
+    if (error) {
+      response.send('file write error');
+    } else {
+      response.send('generated');
+    };
+  });
+});
+
+app.get('/read', function(request, response) {
+  var message = fs.readFileSync('message',{ encoding: 'utf8' });
+  response.send( message );
+});
+
+app.get('/q/:id', function(request, response) {
+
+  phantom.create(function (ph) {
+    ph.createPage(function (page) {
+      console.log( request.params.id );
+      page.open( 'http://' + request.params.id, function (status) {
+        console.log("opened page?", status);
+        page.evaluate(function () { return document.title; }, function (result) {
+          console.log('Page title is ' + result);
+          ph.exit();
+        });
+      });
+    });
+  });
+  response.send( request.params.id );
+});
+
 
 app.get('/', function(request, response) {
   response.send('Hello Node!');
